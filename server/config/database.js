23554e46e4837255f837fc5c347bb959ec0aaa62
Shivang Chauhan <shivangchauhan@Shivangs-MacBook-Air.local>
2024-05-13 10:48:06 +0530

    url change in resetPassword

--- server/config/database.js
+++ server/config/database.js
@@ -4,15 +4,17 @@ require("dotenv").config();
 const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlparser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log(`DB Connection Success`))
+    .catch((err) => {
+      console.log(`DB Connection Failed`);
+      console.log(err);
+      process.exit(1);
+    });
 };
+
+// with the connect method of the mongoose, we pass two arguments, first is the mongoDB_URL and second are the two options
